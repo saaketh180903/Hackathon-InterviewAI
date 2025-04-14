eb0d43e0b26ab19407e7c95d4c3840a89c86ec6d
Harsh <harshku2222@gmail.com>
2025-04-15 03:05:25 +0530

    Login page created and redus implimented on that page

---
+++ react-frontend/src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import signUpImg from "../assets/background-wavy-image.png"
+import Template from "../components/core/Template"
+
+const Signup = () => {
+    return (
+        <Template
+            image={signUpImg}
+            formType="signup"
+        />
+    )
+}
+
+export default Signup

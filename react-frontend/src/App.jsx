04a9afa7fa496c863f315cc964e8430813e720e4
Harsh <harshku2222@gmail.com>
2025-04-14 13:14:47 +0530

    Permissions page created and navbar created

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css"
+
 // Redux
 import { useDispatch, useSelector } from "react-redux"
 // React Router
@@ -9,20 +10,29 @@ import Navbar from "./components/common/Navbar"
 import OpenRoute from "./components/core/auth/OpenRoute"
 import PrivateRoute from "./components/core/auth/PrivateRoute"
 // import Dashboard from "./pages/Dashboard"
+
 // Pages
 import Home from "./pages/Home"
+import Permissions from "./pages/Permissions"
+import Interview from "./pages/Interview"
+import Error from "./pages/Error"
 import { ACCOUNT_TYPE } from "./utils/constants"
 
 function App() {
 
   return (
-    <div className="flex min-h-screen w-screen flex-col bg-blue-200 font-inter">
-    <Navbar />
-    <Routes>
-        <Route path="/" element={<Home />} />
-    </Routes>
-      
+    <div className="flex min-h-screen w-full flex-col bg-gradient-to-b from-white to-[#ABBEFF] font-inter overflow-x-hidden">
+      <Navbar />
+      <div className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/permissions" element={<Permissions />} />
+          <Route path="/interview" element={<Interview />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
     </div>
+
   )
 }
 
